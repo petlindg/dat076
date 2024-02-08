6d5b6068c9dd2172530abfb93d9a7883413639ca
Petter Lindgren <petlindg@student.chalmers.se>
2024-02-08 15:36:14 +0100

    Progress on lab2. Connected front-end to all end-points and imported cors into backend. Todo: frontend test
    co-authored-by: Samuel Karlsson <samolsso@chalmers.se>
    co-authored-by: Andrej Kocijan <kocijan.andrej@gmail.com>
    co-authored-by: Zachris Markström Stenhammar <zachris.stenhammar@gmail.com>

---
+++ client/src/App.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import { AxiosResponse } from 'axios';
+
+interface User {
+  userId : number;
+  userName : String;
+  parsnipsPerClick : number
+  parsnipCount : number
+}
+
+function App() {
+  const [user, setUser] = useState<User | undefined>(undefined);
+  const url : String = "http://localhost:8080/";
+  
+  async function updateUser() {
+    try {
+      const response = await axios.get<User>(url + "user");
+      const newUser : User = response.data;
+      setUser(newUser);
+    } catch (e : any) {
+      console.log(e);
+    }
+  }
+  
+  async function incrementParsnip() {
+    try {
+      await axios.post<User>(url + "user", {});
+      await updateUser();
+    } catch (e : any) {
+      console.log(e);
+    }
+  }
+
+  async function purchasePowerup() {
+    try {
+      const response: AxiosResponse<String, any> = await axios.patch<String>(url + "user", {});
+      if(response.status === 403){
+        alert("Not enoug balance for this purchase")
+      }else if(response.status === 200){
+        await updateUser();
+      }else{
+        alert("Unknown error occured")
+      }
+    } catch (e: any){
+      alert("Not enoug balance for this purchase")
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    document.title = 'Parsnip Puncher';
+    updateUser();
+  });
+
+  return (
+    <div>
+      <div>
+        <p id="lblParsnips">Parsnips: {user?.parsnipCount}</p>
+        <div className="boxing-cursor parsnip-animation" onClick={incrementParsnip}>
+        <img
+          draggable="false"  
+          alt='The main parsnip'
+          src={ require('./assets/images/parsnip.png') }
+        />
+        </div>
+      </div>
+      <p id="lblPPC">{user?.parsnipsPerClick} Parsnips Per Click (PPC)</p>
+      <button onClick={purchasePowerup}>
+        Buy Powerup
+      </button>
+    </div>
+  );
+}
+
+export default App;
--- server/src/start.ts
+++ server/src/start.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { userRouter } from "./router/user";
+import cors from "cors";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/user", userRouter);
