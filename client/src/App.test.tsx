2b1dd76454212174a898b77c941f94c5bb53d3c2
Petter Lindgren <petlindg@student.chalmers.se>
2024-02-15 15:25:52 +0100

    Lab3 complete. Added added tests for changing username on front-end, added end-to-end functionality of changing user name
    co-authored-by: Samuel Karlsson <samolsso@chalmers.se>
    co-authored-by: Andrej Kocijan <kocijan.andrej@gmail.com>
    co-authored-by: Zachris Markström Stenhammar <zachris.stenhammar@gmail.com>-

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import User from './App';
+import axios, { AxiosStatic } from 'axios';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const url : String = "http://localhost:8080/";
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<AxiosStatic>;
+
+test('"Parsnips:" should be on the screen', async () => {
+  render(<App />)
+  expect(screen.getByText(/Buy Powerup/)).toBeInTheDocument();
+  expect(screen.getByText(/Parsnips:/)).toBeInTheDocument();
+})
+
+test('After clicking the submit button axios should have been called', async () => {
+  mockedAxios.patch.mockResolvedValue({
+    status: 201,
+    data : ""
+  });
+  
+  render(<App />)
+
+  const button = screen.getByText(/Submit Username/)
+  const inputField : HTMLInputElement = screen.getByTestId('userNameUpdateInput')
+  fireEvent.change(inputField, { target: {value: "newName"}})
+  expect(inputField.value).toEqual("newName")
+  await fireEvent.click(button);
+  expect(mockedAxios.patch).toHaveBeenCalled();
+})
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, FormEvent } from 'react';
 import './App.css';
 import axios from 'axios';
 import { AxiosResponse } from 'axios';
@@ -12,6 +12,7 @@ interface User {
 
 function App() {
   const [user, setUser] = useState<User | undefined>(undefined);
+  const [newUserName, setNewUserName] = useState<string>("")
   const url : String = "http://localhost:8080/";
   
   async function updateUser() {
@@ -49,6 +50,24 @@ function App() {
     }
   }
 
+  async function changeUsername(e : FormEvent) {
+    e.preventDefault()
+    if(newUserName == "")
+    {
+      alert("New username may not be empty");
+      return;
+    }
+
+    const response = await axios.patch<String>(url + "user", {userName : newUserName})
+
+    if(response.status !== 200) {
+      alert("Unknown error occured")
+    } 
+    
+    setNewUserName("");
+    await updateUser();
+  }
+
   useEffect(() => {
     document.title = 'Parsnip Puncher';
     updateUser();
@@ -57,6 +76,7 @@ function App() {
   return (
     <div>
       <div>
+        <p>Hello {user?.userName}!</p>
         <p id="lblParsnips">Parsnips: {user?.parsnipCount}</p>
         <div className="boxing-cursor parsnip-animation" onClick={incrementParsnip}>
           <img
@@ -70,6 +90,14 @@ function App() {
       <button onClick={purchasePowerup}>
         Buy Powerup
       </button>
+      <p>Change username</p>
+      <label htmlFor="userNameUpdateInput">Input your new username: </label>
+      <form onSubmit={async e => await changeUsername(e)}>
+        <input data-testid="userNameUpdateInput" id="userNameUpdateInput" type="text" value={newUserName} onChange={e => {setNewUserName(e.target.value);}}></input>
+        <button id="userNameUpdateSubmitButton" type="submit">
+          Submit Username
+        </button>
+    </form>
     </div>
   );
 }
