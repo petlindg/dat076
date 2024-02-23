a56964a52df32fdd5ca0cef06f5fdd930b00e12d
Andrej Kocijan <kocijan.andrej@gmail.com>
2024-02-23 16:09:17 +0100

    Added tests for user credentials service

--- server/src/db/__mocks__/conn.ts
+++ server/src/db/__mocks__/conn.ts
@@ -1,7 +1,7 @@
 import mongoose, {Connection, createConnection} from 'mongoose';
 import {MongoMemoryServer} from 'mongodb-memory-server';
 
-async function getMemoryServerConnection() {
+async function getMemoryServerConnection(): Promise<Connection> {
     const mongo: MongoMemoryServer = await MongoMemoryServer.create();
     const uri: string = mongo.getUri();
     return createConnection(uri)
---
+++ server/src/service/userCredentials.test.ts
@@ -0,0 +1,66 @@
+import {describe} from "node:test";
+import {UserCredentials} from "../model/userCredentials";
+import {userCredentialsModel} from "../db/userCredentials.db";
+import {IUserCredentialsService} from "./interfaces/IuserCredentials";
+import {UserCredentialsService} from "./userCredentials";
+import {ObjectId} from "mongodb";
+import mongoose from "mongoose";
+
+afterEach(async () => {
+    await (await userCredentialsModel).deleteMany()
+})
+
+jest.mock("../db/conn")
+const userCredentialsService: IUserCredentialsService = new UserCredentialsService()
+describe("User Credentials Service tests", () => {
+
+    const userName: string = "TestUserName";
+    const email: string = "noumailcist@email.com";
+    const password: string = "plzencodeme";
+
+    it("Creating User Credentials with mongoose works as expected", async () => {
+        const userCredentials: UserCredentials = await (await userCredentialsModel).create({
+            userName: userName,
+            email: email,
+            password: password,
+        })
+
+        expect(userCredentials.id).toBeDefined()
+        expect(userCredentials.userName).toEqual(userName)
+        expect(userCredentials.email).toEqual(email)
+        expect(userCredentials.password).toEqual(password)
+    })
+
+    it("Getting User Credentials, will return correct credentials", async () => {
+        const userId: ObjectId = (await (await userCredentialsModel).create({
+            userName: userName,
+            email: email,
+            password: password,
+        }))._id
+
+        const userCredentials: UserCredentials = await userCredentialsService.getUserCredentials(userId)
+
+        expect(userCredentials.userName).toEqual(userName)
+        expect(userCredentials.email).toEqual(email)
+        expect(userCredentials.password).toEqual(password)
+    })
+
+    it("Updating a user's username, should result in the username being updated", async () => {
+        const userCredentials: UserCredentials = await (await userCredentialsModel).create({
+            userName: userName,
+            email: email,
+            password: password,
+        })
+
+        const newUsername : string = "kosobrin"
+        const success: boolean = await userCredentialsService.changeUsername(userCredentials.id, newUsername)
+
+        expect(success).toBeTruthy()
+
+        const userCredentials2: UserCredentials = await userCredentialsService.getUserCredentials(userCredentials.id)
+        expect(userCredentials2.userName).toEqual(newUsername)
+        expect(userCredentials2.email).toEqual(email)
+        expect(userCredentials2.password).toEqual(password)
+    })
+
+})
