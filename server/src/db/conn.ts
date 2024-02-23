86bb6945dbb8f306433b18b3d456c35778e39584
Andrej Kocijan <kocijan.andrej@gmail.com>
2024-02-23 18:53:20 +0100

    Moved env file to /server

--- server/src/db/conn.ts
+++ server/src/db/conn.ts
@@ -1,5 +1,5 @@
 import {Connection, createConnection} from "mongoose";
-const env = require("../env.json");
+const env = require("../../env.json");
 
 const URI: string = `mongodb+srv://${env.mongoUser}:${env.mongoPassword}@parsnip-puncher.7shlst9.mongodb.net/?retryWrites=true&w=majority&appName=parsnip-puncher`;
 
