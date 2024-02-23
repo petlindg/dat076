a33d81a7a347ccddf6787711d8c4b09d2a418131
Andrej Kocijan <kocijan.andrej@gmail.com>
2024-02-23 16:52:54 +0100

    Removed obsolete files

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import {userRouter} from "./router/user";
 import cors from "cors";
 import {userCredentialsRouter} from "./router/userCredentials";
 import {userDataRouter} from "./router/userData";
@@ -10,7 +9,6 @@ export const app = express();
 app.use(express.json());
 app.use(cors());
 app.get("/", (_, res) => res.status(200).send("Server running...  :)"));
-app.use("/user", userRouter);
 app.use("/userCredentials", userCredentialsRouter);
 app.use("/userData", userDataRouter);
 app.use("/powerupActive", powerupActiveRouter);
