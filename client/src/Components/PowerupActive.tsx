17dff89473b4f548b665ccbeabaa9c969928f7c4
Andrej Kocijan <kocijan.andrej@gmail.com>
2024-02-29 17:02:02 +0100

    Add functions needed for active powerup shop to API

--- client/src/Components/PowerupActive.tsx
+++ client/src/Components/PowerupActive.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import axios from "axios";
-import {baseUrl} from "../App";
-import {basicErrorHandler} from "../Helpers/BasicErrorHandler";
+import {Api} from "../Helpers/Api";
 
 export function PowerupActive({updateUserData}: { updateUserData: () => void; }) {
-    async function purchasePowerup() {
+    async function purchasePowerupActive() {
         // TODO this hardcoded the powerupActiveId, get it dynamically
         // TODO to do that the list of powerups will have to be retrieved from the API
 
-        await axios.post<String>(baseUrl + "userData/purchaseActivePowerUp", {powerupActiveId: "65d8947a15e5748f2ed42b99"})
-            .then(async () => updateUserData())
-            .catch(basicErrorHandler)
+        await Api.purchasePowerupActive("65d8947a15e5748f2ed42b99")
+        updateUserData()
     }
 
     return (
         <div>
-            <button onClick={purchasePowerup}>
+            <button onClick={purchasePowerupActive}>
                 Buy Powerup
             </button>
         </div>
--- client/src/Helpers/Api.ts
+++ client/src/Helpers/Api.ts
@@ -3,6 +3,16 @@ import {UserCredentials} from "../Components/Home";
 import {baseUrl} from "../App";
 import {basicErrorHandler} from "./BasicErrorHandler";
 
+interface PowerupActive {
+    id: string;
+    powerupName: string;
+    basePrice: number;
+    increment: number;
+    parsnipsPerSecond: number;
+    priceForUser: number;
+
+}
+
 export class Api {
 
     static async updateUserCredentials(): Promise<UserCredentials | undefined> {
@@ -15,6 +25,25 @@ export class Api {
             .catch(basicErrorHandler)
 
         return userCredentials
+    }
+
+    static async purchasePowerupActive(powerupActiveId: string): Promise<void> {
+        await axios
+            .post<String>(baseUrl + "userData/purchaseActivePowerUp", {powerupActiveId: powerupActiveId})
+            .catch(basicErrorHandler)
+    }
+
+    static async getPowerupsActiveList(): Promise<PowerupActive[] | undefined> {
+
+        let powerupActiveList : PowerupActive[] | undefined
+
+        await axios.get(baseUrl + "powerUpActive")
+            .then((response) => {
+                powerupActiveList = response.data
+            })
+            .catch(basicErrorHandler)
+
+        return powerupActiveList
     }
 }
 
