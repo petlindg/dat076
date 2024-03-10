e011148b5e146dc0780240d772a9167153fb9088
Andrej Kocijan <kocijan.andrej@gmail.com>
2024-03-10 01:21:40 +0100

    Added logout button

--- client/src/Components/NavigationBar.tsx
+++ client/src/Components/NavigationBar.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import Nav from "react-bootstrap/esm/Nav";
 import Navbar from "react-bootstrap/esm/Navbar";
+import {Api} from "../Helpers/Api";
+import Button from "react-bootstrap/Button";
 
 
-export function NavigationBar() {
+export function NavigationBar({isLoggedIn, setIsLoggedIn}: {isLoggedIn: boolean; setIsLoggedIn: any}) {
+
+    async function logOut(){
+        if(!isLoggedIn)
+            return
+
+        Api.logout()
+        setIsLoggedIn(false)
+    }
+
     return (
         <Navbar className="c2">
             <Nav>
@@ -11,6 +22,7 @@ export function NavigationBar() {
                 <Nav.Link className="navButton c1" href="/stats">Statistics</Nav.Link>
                 <Nav.Link className="navButton c1" href="/settings">Settings</Nav.Link>
             </Nav>
+            {isLoggedIn ? <Button className="logoutButton c1" onClick={logOut}>Log Out</Button> : null }
         </Navbar>
     )
 }
